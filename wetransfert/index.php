368a93a8e003d75a65305ec446db4fa78fca9014
ousmaneacs2018 <ousmane.balde@hotmail.fr>
2018-06-26 11:17:15 +0200

    base 26 06 2018

--- wetransfert/index.php
+++ wetransfert/index.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="wetransfert.css">
-    <title>Wetransfert</title>
+    <title>We transfert</title>
 </head>
 <body>
 
-        <header></header>
+        <header>
+            <p>We transfert</p>
+        </header>
 
         <section></section>
         
