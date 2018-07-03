a4ca5385dffb573a10bb3e402a2665bb992fee2f
erickb21 <erick.beaugendre@gmail.com>
2018-07-03 09:08:07 +0200

    "mes modifs"

---
+++ sources formulaire/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/balapa/pen/XbXVRg.
+
+ A simple step form for customer experience. The purpose of this form is to get the feedback from user for the store.
+
+Thanks for watching!
---
+++ uploadfait.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/wetransfert.css">
+    <title>We transfert</title>
+</head>
+<body onload="depart();">
+
+        <header class="h10vh bgblack clrwhite d_flexcenter">
+            <p>We transfert</p>
+        </header>
+        
+            
+        <section class="d_flexcenter row h80vh">
+
+            <p class="fs100">UPLOAD REUSSI</p>
+
+
+        </section>
+
+ 
+        <footer class="h10vh bgblack clrwhite d_flexcenter">
+            <p> lien lien lien lien</p>
+        </footer>
+
+    <script type="text/javascript" src="js/wetransfert.js"> </script>
+</body>
+</html>
